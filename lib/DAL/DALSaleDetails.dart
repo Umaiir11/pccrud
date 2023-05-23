984a05ee3d21cee54635d1a5c2d8a9fea63b3981
Umair11 <iam.umairimran@gmail.com>
2023-05-23 15:43:57 +0500

    two DAL done

--- lib/DAL/DALSaleDetails.dart
+++ lib/DAL/DALSaleDetails.dart
@@ -4,19 +4,35 @@ import 'package:sqflite/sqflite.dart';
 import '../QueryGen/QueryGenSaleDetails.dart';
 import '../cmModule/DbHelper/DbHelperClass.dart';
 
-class DALSaleDetails extends GetxController {
-  Fnc_CudSaleDetails(List<ModSaleDetailsDB> l_SaleDetailsList) async {
+import 'package:get/get.dart';
+import 'package:pccrud/MVVM/Model/DB/ModSaleDetailsDB.dart';
+import 'package:pccrud/MVVM/Model/DB/ModSaleDB.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../QueryGen/QueryGenSaleDetails.dart';
+import '../QueryGen/QueryGenSaleInfo.dart';
+import '../cmModule/DbHelper/DbHelperClass.dart';
+
+class DAL_PC extends GetxController {
+  Fnc_CudSaleDetails(List<ModSaleDetailsDB> l_SaleDetailsList, ModSaleDB l_ModSaleDB) async {
     Database? l_Database = await DBHelper().FncGetDatabaseIns();
-    List<String> l_InsertionQuery = await QueryGenSaleDetails().FncGenCrudQueriesSaleDetails(l_SaleDetailsList);
+
+    List<String> l_SaleDetailsQueries = await QueryGenSaleDetails().FncGenCrudQueriesSaleDetails(l_SaleDetailsList);
+    List<String> l_SaleInfoQueries = await QueryGenSaleInfo().FncGenCrudQueriesSaleInfo(l_ModSaleDB);
 
     final batch = l_Database!.batch();
-    for (int indexoflist = 0; indexoflist < l_InsertionQuery.length; indexoflist++) {
-      final query = l_InsertionQuery[indexoflist];
+
+    for (String query in l_SaleDetailsQueries) {
       batch.execute(query);
     }
+
+    for (String query in l_SaleInfoQueries) {
+      batch.execute(query);
+    }
+
     await batch.commit();
 
-    // Clear the list after the data is stored on the database.
+    // Clear the lists after the data is stored in the database.
     l_SaleDetailsList.clear();
   }
 }
---
+++ lib/DAL/DAL_PC.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+import 'package:pccrud/MVVM/Model/DB/ModSaleDetailsDB.dart';
+import 'package:sqflite/sqflite.dart';
+import '../QueryGen/QueryGenSaleDetails.dart';
+import '../cmModule/DbHelper/DbHelperClass.dart';
+
+class DAL_PC extends GetxController {
+  Fnc_CudSaleDetails(List<ModSaleDetailsDB> l_SaleDetailsList) async {
+    Database? l_Database = await DBHelper().FncGetDatabaseIns();
+    List<String> l_InsertionQuery = await QueryGenSaleDetails().FncGenCrudQueriesSaleDetails(l_SaleDetailsList);
+
+    final batch = l_Database!.batch();
+    for (int indexoflist = 0; indexoflist < l_InsertionQuery.length; indexoflist++) {
+      final query = l_InsertionQuery[indexoflist];
+      batch.execute(query);
+    }
+    await batch.commit();
+
+    // Clear the list after the data is stored on the database.
+    l_SaleDetailsList.clear();
+  }
+}
