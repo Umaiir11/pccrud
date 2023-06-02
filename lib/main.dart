ec07e8425e5772b45660c4b8e387c32b941470cc
Umair11 <iam.umairimran@gmail.com>
2023-06-02 13:09:59 +0500

    Custom Widgetqwasi11

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
         useMaterial3: true,
       ),
       home: const VwSale(),
