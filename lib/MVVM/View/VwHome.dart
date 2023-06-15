a8dbe14dde531d2b2d4e9984fb51c32329249a97
Umair11 <iam.umairimran@gmail.com>
2023-06-15 16:08:25 +0500

    ddata

--- lib/MVVM/View/VwHome.dart
+++ lib/MVVM/View/VwHome.dart
@@ -47,7 +47,7 @@ class _VwHomeState extends State<VwHome> {
                 child: ElevatedButton(
                     onPressed: () {
                       l_VmSale.FncNewForm(TextEditingController(),TextEditingController());
-                      Get.offNamedUntil(AppRoutes.VwSale, ModalRoute.withName(AppRoutes.initialRoute));
+                      Get.toNamed(AppRoutes.VwSale);
                     },
                     child: const Text("CREATE!")),
               ),
