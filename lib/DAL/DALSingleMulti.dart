423a9d1f4a4512ed3314b8c54358ee0d69f284cb
Umair11 <iam.umairimran@gmail.com>
2023-06-23 12:31:29 +0500

    ndw

--- lib/DAL/DALSingleMulti.dart
+++ lib/DAL/DALSingleMulti.dart
@@ -1,8 +1,6 @@
 import 'package:get/get.dart';
-import 'package:pccrud/MVVM/Model/DB/ModPcSale.dart';
 import 'package:pccrud/MVVM/Model/DB/ModSingleMulti.dart';
 import 'package:sqflite/sqflite.dart';
-import '../QueryGen/QueryGenSalePC.dart';
 import '../QueryGen/QueryGenSingleMulti.dart';
 import '../cmModule/DbHelper/DbHelperClass.dart';
 
@@ -10,7 +8,7 @@ class DAL_SingleMulti extends GetxController {
   Future<bool> Fnc_Cud( List<ModSingleMulti> l_listModSingleMulti) async {
     try {
       Database? lDatabase = await DBHelper().FncGetDatabaseIns();
-      List<String> l_Quries = await QueryGenSingleMulti().FncGenCrudQueriesSaleDetailsList(l_listModSingleMulti);
+      List<String> l_Quries = await   QueryGenSingleMulti().FncGenCrudQueriesSaleDetailsList(l_listModSingleMulti);
       final batch = lDatabase!.batch();
 
       for (String query in l_Quries) {
